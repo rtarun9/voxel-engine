a4646f7ef7b4d6fdcc64500b4f25d7ffa4397723
rtarun9 <rtarun0913@gmail.com>
2024-06-09 18:48:20 +0530

    Removed all files from project

--- include/shader_compiler.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// The naming convention of namespaces is being broken here, mostly because this
-// "namespace" is a smart way of simulating static class behaviour.
-// DXC is used for shader compilation.
-namespace ShaderCompiler
-{
-IDxcBlob *compile(const wchar_t *const file_path, const wchar_t *const entry_point, const wchar_t *const target);
-}
